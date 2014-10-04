d920f840bb35d2002942a541f0491e0e898b7fb2
Tyler Olson <tydotg@gmail.com>
2014-10-03 17:56:44 -0700

    Circle controls

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+$(function() {
+
+  $('.circle-controls li').click(function(e) {
+    var target = $(e.target),
+      id = target.attr('id');
+
+    Circles[id].call(Circles);
+
+    if(id === 'pause') {
+      target.text('>');
+      target.attr({id: 'start'});
+    } else if(id === 'start') {
+      target.text('=');
+      target.attr({id: 'pause'});
+    }
+  });
+
+
+});
+
 console.log('bleep blop bloop');
